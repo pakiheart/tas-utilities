acd124a528e02cf96e0ee8655d68d9f8c6891219
Rehan Altaf <pakiheart@gmail.com>
2024-08-19 10:51:08 -0600

    updated to add adding org_mgr to space aud

--- tas_scripts/manager_to_auditor_role_switch_ldap_users.sh
+++ tas_scripts/manager_to_auditor_role_switch_ldap_users.sh
@@ -3,7 +3,8 @@
 #login into cf cli
 # Needs yq cli
 
-# This will move ldap_users from manager to auditor
+# This will move ldap_users from manager to auditor and 
+# add missing orginal org_manager users as space org_auditors so they do not use access
 
 if [ $# -eq 0 ]
   then
@@ -39,22 +40,13 @@ for org in $orgs; do
     org_ldap_users=$(cat $Path_To_Config_Dir/config/$org_name/orgConfig.yml | yq -r '."org-manager".ldap_users.[]')
 
     for org_ldap_user in $org_ldap_users; do
-        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            yq -i 'del(."org-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-            yq -i '."org-auditor".ldap_users += ['"\"$org_ldap_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-        elif [[ "$OSTYPE" == "darwin"* ]]; then
-            yq -iy 'del(."org-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-            yq -iy '."org-auditor".ldap_users += ['"\"$org_ldap_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-        fi
+        yq -i 'del(."org-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/orgConfig.yml
+        yq -i '."org-auditor".ldap_users += ['"\"$org_ldap_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
     done
     remove_dup_org_users=$(cat $Path_To_Config_Dir/config/$org_name/orgConfig.yml | yq -r '."org-auditor".ldap_users | unique' | yq .[])
     yq -i 'del(."org-auditor".ldap_users)' $Path_To_Config_Dir/config/$org_name/orgConfig.yml
     for remove_dup_org_user in $remove_dup_org_users; do
-        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            yq -i '."org-auditor".ldap_users += ['"\"$remove_dup_org_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-        elif [[ "$OSTYPE" == "darwin"* ]]; then
-            yq -iy '."org-auditor".ldap_users += ['"\"$remove_dup_org_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
-        fi
+        yq -i '."org-auditor".ldap_users += ['"\"$remove_dup_org_user\""']'  $Path_To_Config_Dir/config/$org_name/orgConfig.yml
     done
   fi
   total_pages=$(cf curl '/v2/organizations/'"$org_guid"'/spaces' | jq -r .total_pages)
@@ -77,22 +69,16 @@ for org in $orgs; do
       space_ldap_users=$(cat $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml | yq -r '."space-manager".ldap_users.[]')
 
       for space_ldap_user in $space_ldap_users; do
-          if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-              yq -i 'del(."space-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-              yq -i '."space-auditor".ldap_users += ['"\"$space_ldap_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-          elif [[ "$OSTYPE" == "darwin"* ]]; then
-              yq -iy 'del(."space-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-              yq  -iy '."space-auditor".ldap_users += ['"\"$space_ldap_user\""']' $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-          fi
+          yq -i 'del(."space-manager".ldap_users)' $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
+          yq -i '."space-auditor".ldap_users += ['"\"$space_ldap_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
+      done
+      for remove_dup_org_user in $remove_dup_org_users; do
+        yq -i '."space-auditor".ldap_users += ['"\"$remove_dup_org_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
       done
       remove_dup_space_users=$(cat $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml | yq -r '."space-auditor".ldap_users | unique' | yq .[])
       yq -i 'del(."space-auditor".ldap_users)' $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
       for remove_dup_space_user in $remove_dup_space_users; do
-          if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-              yq -i '."space-auditor".ldap_users += ['"\"$remove_dup_space_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-          elif [[ "$OSTYPE" == "darwin"* ]]; then
-              yq -iy '."space-auditor".ldap_users += ['"\"$remove_dup_space_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
-          fi
+          yq -i '."space-auditor".ldap_users += ['"\"$remove_dup_space_user\""']'  $Path_To_Config_Dir/config/$org_name/$space_name/spaceConfig.yml
       done
     fi
   done
